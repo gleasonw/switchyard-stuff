cc7575a3b0ab1d6f14ec6d6a40c4d8f81f4207fd
nicole-binder <bindern@carleton.edu>
2020-10-07 08:54:30 -0500

    added some framework code

--- myhub.py
+++ myhub.py
@@ -1,4 +1,5 @@
 from switchyard.lib.userlib import *
+import datetime
 
 def main(net):
     # constant: forwarding table size (int)
@@ -10,6 +11,8 @@ def main(net):
         log_info ("{}: ethernet address {}".format(port.name, port.ethaddr))
         # make a list of our hub addresses so we drop requests coming to us
     #start timer
+    forwarding_table = {}
+    time_table = {}
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
@@ -20,15 +23,26 @@ def main(net):
             # try again...
             continue
 
+        header = packet[0]
+
+        #should we check for timedout entries here? or after we had new to table
+
+        if header.src not in forwarding_table:
+            record_address(port, header, forwarding_table)
+            record_timestamp(header, time_table)
+
+
         # Check if destination in our list of hub addresses
             # If yes, drop it
             # if not, check if in our forwarding table
                 # if yes, send it there and update forwarding table (different function?)
+                    #record_address(port, header, forwarding_table)
                 # if not, broadcast
                     #sendAll(net, input_port)
 
         # new function SendToAll? Then SendSpecific(proper address)?
         # check if any entries have timed out
+        #remove_timed_out(time_table, forwarding_table)
 
         # shutdown is the last thing we should do
 
@@ -44,5 +58,15 @@ def sendAll(net, inputPort, packet):
 def sendSpecific(net, destPort, packet):
     net.send_packet(destPort, packet)
 
+def record_address(port, header, forwarding_table):
+    #check if it is has timedout
+    forwarding_table.update(header.src, port)
 
+def record_timestamp(header, time_table):
+    time_table.update(header.src, datetime.datetime.now().time())
+
+def remove_timed_out(time_table, forwarding_table):
+    for k,v in time_table:
+        if time_table.get(k) - datetime.datetime.now().time > 30:
+            forwarding_table.pop(k)
 
